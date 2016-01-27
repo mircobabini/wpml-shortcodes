cd24fb36fe539f662769a233a8ef15e1454c63af
Mirco Babini <mirkolofio@gmail.com>
2016-01-27 10:23:21 +0100

    1.2.5

--- wpml-shortcodes.php
+++ wpml-shortcodes.php
@@ -4,52 +4,30 @@ Plugin Name: WPML Shortcodes
 Plugin URI: http://github.com/mircobabini/wpml-shortcodes
 Description: Adds shortcodes to the WPML environment, like wpml__, wpml_e and more. Make your WordPress <strong>full WPML ready</strong>.
 Author: Mirco Babini
-Version: 1.2.4
+Version: 1.2.5
 Author URI: http://github.com/mircobabini
 */
-defined('ABSPATH') or die("No script kiddies please!");
+( ! defined( 'ABSPATH' ) ) && exit;
 if( defined('WPML_SHORTCODES') ) return;
 
 /* here we go */
 define( 'WPML_SHORTCODES', 1 );
 
-function wpml_icl_t__shortcode( $attr, $content = null ){
-	if( $content === null ){
-		return '';
-	}
+/* support optional context */
+function wpml__main_context( $context = null ){
+    global $wpml__main_context;
 
-	extract(shortcode_atts(array(
-		'context' => null,
-		'name' => null,
-	), $attr));
+    if( $context === null ){
+        return $wpml__main_context;
+    }
 
-	if( $context === null ){
-		return $content;
-	}
-
-	return icl_t( $context, $name, $content );
+    $wpml__main_context = $context;
+    return true;
 }
-add_shortcode( 'wpml_icl_t', 'wpml_icl_t__shortcode' );
+wpml__main_context( 'default' );
 
-function wpml__shortcode( $attr, $content = null ){
-	if( $content === null ){
-		return '';
-	}
-
-	extract(shortcode_atts(array(
-		'context' => null,
-		'name'    => null,
-	), $attr));
-
-	if( $context === null ){
-		return $content;
-	}
-
-	return wpml__( $content, $context, $name );
-}
-add_shortcode( 'wpml__', 'wpml__shortcode' );
-
-function wpml__( $string, $context, $name = null ){
+/* main api */
+function wpml__( $string, $context = null, $name = null ){
 	_icl_register_string( $string, $context, $name );
 	$translation = _icl_t( $string, $context, $name );
 	return $translation;
@@ -59,8 +37,26 @@ function wpml_e( $string, $context, $name = null ){
 	echo $translation;
 }
 
-function _icl_t( $string, $context, $name = null ){
+/* support optional name, context */
+function _icl_ensure_name( $name, $string ){
+	if( $name === null ){
+		$name = sha1( $string );
+	}
+
+	return $name;
+}
+function _icl_ensure_context( $context ){
+    if( $context === null ){
+        $context = wpml__main_context();
+    }
+
+    return $context;
+}
+
+/* wrappers to work even without wpml */
+function _icl_t( $string, $context = null, $name = null ){
 	$name = _icl_ensure_name( $name, $string );
+    $context = _icl_ensure_context( $context );
 
 	if( function_exists('icl_t') ){
 		$translation = icl_t( $context, $name, $string );
@@ -76,14 +72,8 @@ function _icl_register_string( $string, $context, $name = null ){
 	}
 
 	$name = _icl_ensure_name( $name, $string );
+    $context = _icl_ensure_context( $context );
 	return icl_register_string( $context, $name, $string );
-}
-function _icl_ensure_name( $name, $string ){
-	if( $name === null ){
-		$name = sha1( $string );
-	}
-
-	return $name;
 }
 
 /* ex. WPML Translate Shortcode */
@@ -96,17 +86,20 @@ function wpml_if__shortcode( $attr, $content = null ){
 		return '';
 	}
 
-	return wpml_e__if_language( $content, $lang );
+	return wpml____if_language( $content, $lang );
 }
 add_shortcode( 'wpml_if', 'wpml_if__shortcode' );
 
 /* tools */
-function wpml_e__if_language( $content, $lang ){
+function wpml____if_language( $content, $lang ){
 	if ( wpml_secure__get_current_language() === $lang ){
 		return do_shortcode( $content );
 	} else{
 		return '';
 	}
+}
+function wpml_e__if_language( $content, $lang ){
+	echo wpml____if_language( $content, $lang );
 }
 function wpml_secure__get_current_language(){
 	if( defined( 'ICL_LANGUAGE_CODE') ){
@@ -117,3 +110,31 @@ function wpml_secure__get_current_language(){
 
 	return $lang;
 }
+
+/* shortcodes */
+function wpml_icl_t__shortcode( $attr, $content = null ){
+	if( $content === null ){
+		return '';
+	}
+
+	extract(shortcode_atts(array(
+		'context' => null,
+		'name' => null,
+	), $attr));
+
+	return _icl_t( $content, $context, $name );
+}
+function wpml__shortcode( $attr, $content = null ){
+	if( $content === null ){
+		return '';
+	}
+
+	extract(shortcode_atts(array(
+		'context' => null,
+		'name'    => null,
+	), $attr));
+
+	return wpml__( $content, $context, $name );
+}
+add_shortcode( 'wpml_icl_t', 'wpml_icl_t__shortcode' );
+add_shortcode( 'wpml__', 'wpml__shortcode' );
